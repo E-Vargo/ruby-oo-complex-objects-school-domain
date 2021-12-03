a5d0553df5337896c4346ef59d9cdd14da1ffcb3
E-Vargo <vargoelliott@gmail.com>
2021-12-02 19:00:49 -0800

    commit tree 92f50998350e4b875b504e43e835c58bfe504b1c

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+    
+
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+
+def roster
+@roster
+end
+
+def add_student(student_name, grade)
+    if @roster.keys.include?(grade) == false
+@roster[grade] = []
+@roster[grade] << student_name
+    else
+@roster[grade] << student_name
+    end
+end
+
+def grade(num)
+    @roster[num]
+end
+
+def sort
+    @roster.each do |key, value|
+        value.sort!
+    end
+end
+
+
+
+end
